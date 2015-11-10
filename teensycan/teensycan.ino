e51e42ff1cf13efc489a9e20a2d31df94fd3e945
JPitty <jjpittinger@gmail.com>
2015-11-09 17:10:32 -0700

    Add can and serial

---
+++ teensycan/teensycan.ino
@@ -0,0 +1,168 @@
+//Arduino 1.6.5, 11/9/15 JP
+//Uses Teensy 3.2 CAN bus, intended to be mounted on CAN_Node v0.2 or eq.
+//to control an AMIS-30543 stepper motor controller using a Pololu dev board #2970.
+//Teensy 3.2 hw SPI pins: 
+// 11= Data Out
+// 12= Data In
+// 13= Clock
+
+//#include <Metro.h>
+#include <FlexCAN.h>
+#include <SPI.h>
+#include <AMIS30543.h>
+
+//Metro sysTimer = Metro(1);// milliseconds
+const uint8_t stepPin = 9;  //pulse per step to AMIS
+const uint8_t ss = 10;  //slave select
+
+AMIS30543 stepper;
+FlexCAN CANbus(500000);
+static CAN_message_t msg,rxmsg;
+//static uint8_t hex[17] = "0123456789abcdef";
+static CAN_filter_t myMask, myFilter_0;
+
+//int txCount,rxCount;
+//unsigned int txTimer,rxTimer;
+
+//on-board RGB LED indicator:
+const int redPin =  23;
+const int greenPin =  21;
+const int bluePin =  22;
+int i;
+int debug = 1;
+
+void setup(void)
+{
+  //CAN addressing
+  myMask.rtr = 0;
+  myMask.ext = 0;
+  myMask.id = 0xfff;  //masks everything except exact filter.id
+  CANbus.begin(myMask);
+  myFilter_0.id = 0x003; //this should be read from hw: dips, etc
+  myFilter_0.ext = 0;
+  myFilter_0.rtr = 0;
+  CANbus.setFilter(myFilter_0,0);
+  CANbus.setFilter(myFilter_0,1);
+  CANbus.setFilter(myFilter_0,2);
+  CANbus.setFilter(myFilter_0,3);
+  CANbus.setFilter(myFilter_0,4);
+  CANbus.setFilter(myFilter_0,5);
+  CANbus.setFilter(myFilter_0,6);
+  CANbus.setFilter(myFilter_0,7);
+
+  SPI.begin();
+  stepper.init(ss);
+  digitalWrite(stepPin, LOW);
+  pinMode(stepPin, OUTPUT);
+  delay(1);
+  
+  stepper.resetSettings();
+  stepper.setCurrentMilliamps(1500);
+  stepper.setStepMode(4); //1,2,4,8,16,32,64,128
+  stepper.enableDriver();
+  
+  Serial.begin(9600);
+  delay(100);
+  Serial.println(F("Hello Teensy 3.1 CAN."));
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  analogWrite(redPin, 30);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 20);
+  delay(500);
+
+  //sysTimer.reset();
+}
+
+void loop(void)
+{ 
+//  msg.len = 8;
+//  msg.id = 0x222;
+//  for( int idx=0; idx<8; ++idx ) {
+//    msg.buf[idx] = '0'+idx+1;
+//  }
+
+  if ( CANbus.available() ){
+    CANbus.read(rxmsg);
+    
+    if (rxmsg.len >= 3) {
+      analogWrite(redPin, rxmsg.buf[0]);
+      analogWrite(bluePin, rxmsg.buf[1]);
+      analogWrite(greenPin, rxmsg.buf[2]);
+    }
+
+    if ( debug > 0 ) {
+      Serial.print("sender id: ");
+      Serial.println(rxmsg.id);
+      //for loop through data buf
+      for (i=0; i<rxmsg.len; i++) {
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(rxmsg.buf[i]);
+      }
+    }
+  }
+
+  int stepSpeed = 250;
+  // Step in the default direction
+  setDirection(0);
+  for (unsigned int x = 0; x < 2400; x++)
+  {
+    step(stepSpeed);
+  }
+  // Wait for 300 ms.
+  delay(300);
+  // Step in the other direction
+  setDirection(1);
+  
+  for (unsigned int x = 0; x < 2400; x++)
+  {
+    step(stepSpeed);
+  }
+  
+  delay(300);
+}
+
+// Sends a pulse on the NXT/STEP pin to tell the driver to take
+// one step, and also delays to control the speed of the motor.
+void step(int speedDelay)
+{
+  // The NXT/STEP minimum high pulse width is 2 microseconds.
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(3);
+
+  // The delay here controls the stepper motor's speed.  You can
+  // increase the delay to make the stepper motor go slower.  If
+  // you decrease the delay, the stepper motor will go fast, but
+  // there is a limit to how fast it can go before it starts
+  // missing steps.
+  //REPLACE so this isn't blocking:
+  delayMicroseconds(speedDelay);
+}
+
+// Writes a high or low value to specify what direction to turn the motor.
+void setDirection(bool dir)
+{
+  // The NXT/STEP pin must not change for at least 0.5
+  // microseconds before and after changing the DIR pin.
+  //?? Is this true for sw dir change with SPI?
+  delayMicroseconds(1);
+  stepper.setDirection(dir);
+  delayMicroseconds(1);
+}
+
+/*static void hexDump(uint8_t dumpLen, uint8_t *bytePtr)
+{
+  uint8_t working;
+  while( dumpLen-- ) {
+    working = *bytePtr++;
+    Serial.write( hex[ working>>4 ] );
+    Serial.write( hex[ working&15 ] );
+  }
+  Serial.write('\r');
+  Serial.write('\n');
+}*/
---
+++ teensyserial/teensyserial.ino
@@ -0,0 +1,231 @@
+//Ard. 1.0.5, 1.6.5 11/9/15 JP
+//This version doesn't use Accel, too many missed steps
+//send an cmd over the serial cnxn, motor should do what it's told.
+//Uses Teensy 3.2 to control an AMIS-30543 stepper motor controller using a Pololu dev board #2970.
+//Teensy 3.2 hw SPI pins: 
+// 11= Data Out
+// 12= Data In
+// 13= Clock
+
+//#include <Metro.h>
+#include <SPI.h>
+#include <AMIS30543.h>
+
+//Metro sysTimer = Metro(1);// milliseconds
+const uint8_t stepPin = 9;  //pulse per step to AMIS
+const uint8_t ss = 10;  //slave select
+
+AMIS30543 stepper;
+int debug = 1;
+
+int stepSpeed = 350; //delay in microsec.
+int stepSpeedt = stepSpeed;
+int stepRampup = 0;
+int totSteps = 0;
+int stepCount = 0;
+int runto=0;
+int newcmd=0;
+int runtime=0;
+int steps=0;
+boolean setdir, dir, setspd, setrt, setmode, setcont;
+unsigned long time, dur;
+int stepRampup = (400-stepSpeed)/100;
+
+enum stepsetting {
+1,2,4,8,16,32,64,128
+};  //usage: stepsetting variable = <value>
+
+void setup() {
+  SPI.begin();
+  stepper.init(ss);
+  digitalWrite(stepPin, LOW);
+  pinMode(stepPin, OUTPUT);
+  delay(1);
+  
+  stepper.resetSettings();
+  stepper.setCurrentMilliamps(1500);
+  stepper.setStepMode(4); //1,2,4,8,16,32,64,128... ENUM?
+  stepper.enableDriver();
+  
+  Serial.begin(9600);
+  Serial.println("Hello Teensy 3.1 Serial");
+
+  //sysTimer.reset();
+}
+
+void loop() {  
+  //read the new Serial command
+  if (Serial.available()) {
+    runto = 0;
+    setspd = false;
+    setrt = false;
+    setdir = false;
+    setmode = false;
+	//setpwr = false;  ADD BELOW to set motor current with stepper.setCurrentMilliamps()
+    while (Serial.available()) {
+      char c = Serial.read();
+      if (c == 'd') { //set direction
+        setdir = true;
+      } else if (c == 's') { //start msg w/ 's' to set speed
+        setspd = true;
+      } else if (c == 'g') { //'g' to set runtime in millis
+        setrt = true;
+      } else if (c == 'm') { //'m' to set microsteps
+        setmode = true;
+      } else if (c == 'x') { //'x' to set cont run
+        setcont = true;
+      } else {
+        if (c >= '0' && c <= '9') {
+          runto = (10 * runto) + (c - '0') ; // convert string of digits to a number
+        }
+    }}
+    
+    if (setspd){
+      stepSpeed = runto;
+      newcmd=4;
+    } else if (setrt) {
+      runtime = runto;
+      newcmd=2;
+    } else if (setdir) {
+      if (runto == 0) {
+        dir = false;
+      } else {
+        dir = true;
+      }
+	  delayMicroseconds(1);
+      stepper.setDirection(dir);
+      delayMicroseconds(1);
+      newcmd=3;
+    } else if (setmode) {
+        if (runto > 0 ) {  //TODO: check for valid setting, should this be an ENUM?
+          stepper.setStepMode(runto);
+        } else {
+          stepper.setStepMode(4); //default
+        }
+      newcmd=5;
+    } else if (setcont) {
+      newcmd=6;
+    } else {
+      totSteps = runto;
+      newcmd=1;
+    }
+  }
+  
+  //move number of steps
+  if (newcmd==1) { 
+    Serial.print("moving: ");
+    Serial.println(runto);
+    digitalWrite(11,HIGH);
+    digitalWrite(10,LOW);
+    stepCount = 0;
+    stepSpeedt=400;
+	
+    //ramp-up
+    while (stepCount < totSteps){
+      if (stepSpeed < 350 && stepSpeedt > stepSpeed){ 
+        stepSpeedt=400-stepCount*stepRampup;
+      } else {
+        stepSpeedt = stepSpeed;
+      }
+	  
+      step(stepSpeedt);
+	  stepCount++;
+      if (Serial.available()) {
+        Serial.flush();
+        Serial.println("interrupted by serial line");
+        break;
+      }
+    }
+    delay(50);
+    Serial.print("new pos: ");
+    Serial.println(totSteps);
+	
+    //move for a duration
+  } else if (newcmd==2) {  
+    digitalWrite(10,LOW);
+    time = millis();
+    Serial.print("Time: ");
+    Serial.println(time);
+    dur = time + runtime;
+    stepCount = 0;
+    stepSpeedt=400;
+    while (time < dur) {
+      if (stepSpeed < 350 && stepSpeedt > stepSpeed){ //ramp-up
+        stepSpeedt=400-stepCount*stepRampup;
+        stepCount++;
+      } else {
+        stepSpeedt = stepSpeed;
+      }
+      digitalWrite(0,HIGH);
+      delayMicroseconds(stepSpeedt);
+      digitalWrite(0,LOW);
+      delayMicroseconds(stepSpeedt); 
+      time = millis();
+      if (Serial.available()) {
+        Serial.flush();
+        Serial.println("interrupted by serial line");
+        break;
+      }
+    }
+    Serial.print("Time: ");
+    time = millis();
+    Serial.println(time);
+    delay(250);
+    digitalWrite(10,HIGH);
+    
+  } else if (newcmd==3) { //set direction
+      digitalWrite(1,dir);
+      Serial.print("direction: ");
+      Serial.println(dir);
+  } else if (newcmd==4) {
+      Serial.print("new speed: ");
+      Serial.println(stepSpeed);
+  } else if (newcmd==5) {
+      Serial.print("micro step div: ");
+      Serial.println(steps);
+  } else if (newcmd==6) {
+      Serial.println("running continous");
+      digitalWrite(11,HIGH);
+      digitalWrite(10,LOW);
+      while (1) {
+        if (stepSpeed < 350 && stepSpeedt > stepSpeed){ //ramp-up
+          stepSpeedt=400-stepCount*stepRampup;
+          stepCount++;
+        } else {
+          stepSpeedt = stepSpeed;
+        }
+        digitalWrite(0,HIGH);
+        delay(50);
+        digitalWrite(0,LOW);
+        delay(50);
+        if (Serial.available()) {
+          Serial.flush();
+          Serial.println("interrupted by serial line");
+          setcont = false;
+          digitalWrite(10,HIGH);
+          digitalWrite(11,LOW);
+          break;
+        }
+      }
+  }
+  newcmd=0;
+}
+
+// Sends a pulse on the NXT/STEP pin to tell the driver to take
+// one step, and also delays to control the speed of the motor.
+void step(int speedDelay)
+{
+  // The NXT/STEP minimum high pulse width is 2 microseconds.
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(3);
+
+  // The delay here controls the stepper motor's speed.  You can
+  // increase the delay to make the stepper motor go slower.  If
+  // you decrease the delay, the stepper motor will go fast, but
+  // there is a limit to how fast it can go before it starts
+  // missing steps.
+  //REPLACE so this isn't blocking:
+  delayMicroseconds(speedDelay);
+}
